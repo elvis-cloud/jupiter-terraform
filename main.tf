b95e31248fab6154964b2c40999ec80438ec01d9
Elvis Vukesu <elvisvukesu@icloud.com>
2023-06-28 16:20:31 -0700

    redirect corrected

--- main.tf
+++ main.tf
@@ -265,7 +265,6 @@ resource "aws_alb_listener" "dev-https-listener" {
   load_balancer_arn = aws_alb.dev-alb.arn
   port = "80"
   protocol = "HTTP"
-  ssl_policy = "ELBSecurityPolicy-2016-08"
   certificate_arn = "arn:aws:acm:us-east-1:463570358144:certificate/9bffa55b-bb97-4845-8e65-25fbc227ebfd"
 
   default_action {
