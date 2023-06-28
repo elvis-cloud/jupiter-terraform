ad697d3e090910b01cf3c5b77d8bb28d3ab9dfdc
Elvis Vukesu <elvisvukesu@icloud.com>
2023-06-28 16:18:10 -0700

    redirect corrected

--- main.tf
+++ main.tf
@@ -263,33 +263,22 @@ resource "aws_alb_target_group" "dev-tg" {
 
 resource "aws_alb_listener" "dev-https-listener" {
   load_balancer_arn = aws_alb.dev-alb.arn
-  port = "443"
-  protocol = "HTTPS"
+  port = "80"
+  protocol = "HTTP"
   ssl_policy = "ELBSecurityPolicy-2016-08"
   certificate_arn = "arn:aws:acm:us-east-1:463570358144:certificate/9bffa55b-bb97-4845-8e65-25fbc227ebfd"
 
   default_action {
-    type = "forward"
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+
     target_group_arn = aws_alb_target_group.dev-tg.arn
   }
 }
 
-resource "aws_alb_listener_rule" "http-to-https-rule" {
-  listener_arn = aws_alb_listener.dev-https-listener.arn
-  action {
-    type = "redirect"
-
-    redirect {
-      protocol    = "HTTPS"
-      port        = "443"
-      status_code = "HTTP_301"
-    }
-  }
-
-  condition {
-    host_header {
-      values = ["my-service.*.terraform.io"]
-    }
-  }
-}
 
