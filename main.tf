931c8a2dbfe021f4f0206465988af5da54174822
Elvis Vukesu <elvisvukesu@icloud.com>
2023-06-28 14:35:02 -0700

    added sg to lt

--- main.tf
+++ main.tf
@@ -211,7 +211,7 @@ resource "aws_security_group" "webserver-sg" {
 resource "aws_launch_template" "dev-app-lt" {
   image_id = "ami-090e0fc566929d98b"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webserver-sg]
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   tags = {
     "Name" = "app-server-lt"
   }
